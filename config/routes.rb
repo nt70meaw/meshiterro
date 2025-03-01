b18253fc03161b829e74fec2ff29f8c9cfba9a95
nt70meaw <181>
2025-03-01 16:00:42 +0000

    sixth commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get "/homes/about" => "homes#about", as: "about"
+  resources :users, only: [:show, :edit, :update]
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
 end
